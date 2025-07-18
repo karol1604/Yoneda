330acb4f3391f5920b6ccea87205a8752e6247a5
karol1604 <karol.gromada16@gmail.com>
2025-07-18 23:44:08 +0200

    still dont know git

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-fn main() {
-    println!("Starting evaluation of lambda expressions...");
-}
-
 mod term;
+use term::{app, eval_dbr, from_debruijn, lam, var};
+
 #[cfg(test)]
 mod tests;
+
+fn main() {
+    println!("Starting evaluation of lambda expressions...");
+
+    let id = lam("x", var("x"));
+    let id_app = app(id, var("y"));
+    let result = eval_dbr(id_app);
+    let mut ctx = vec![];
+    println!("=> {}", from_debruijn(&result, &mut ctx));
+}
