b2eb35218d0fcda721a4751f1b6ca94b2ddc063f
karol1604 <karol.gromada16@gmail.com>
2025-07-25 19:53:44 +0200

    fix tests lol

--- src/tests/pratt.rs
+++ src/tests/pratt.rs
@@ -1,16 +1,16 @@
-
-use crate::parser::{parse_expr};
-use crate::lexer::{Expr, Lexer};
+use crate::lexer::Lexer;
+use crate::parser::parse_expr;
 use pretty_assertions::assert_eq;
 
 #[test]
 fn tests() {
     let mut lexer = Lexer::new("λx.x");
     let expr = parse_expr(&mut lexer);
-    assert_eq!(expr.to_string(),"(λx.x)");
+    assert_eq!(expr.to_string(), "λx.x");
 
-    let mut lexer = Lexer::new("((λx.(x x)) (λx.(x x)))");
+    let mut lexer = Lexer::new("(λx.x x) (λx.x x)");
     let expr = parse_expr(&mut lexer);
-    assert_eq!(expr.to_string(),"((λx.(x x)) (λx.(x x)))");
+    assert_eq!(expr.to_string(), "(λx.x x) (λx.x x)");
     println!("{}", expr);
 }
+
